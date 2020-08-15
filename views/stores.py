20786f0939a16660d32edd9c05aecb9ffeadc1a9
NikelausM <jnickm@gmail.com>
2020-08-15 12:54:25 -0600

    Add delete stores

--- views/stores.py
+++ views/stores.py
@@ -31,3 +31,34 @@ def new():
         return redirect(url_for('.index'))
 
     return render_template('stores/new.html')
+
+
+@store_blueprint.route('/edit/<string:store_id>', methods=['GET', 'POST'])
+def edit(store_id):
+    store = Store.get_by_id(store_id)
+
+    if request.method == 'POST':
+        name = request.form['name']
+        url_prefix = request.form['url_prefix']
+        tag_name = request.form['tag_name']
+        query = json.loads(request.form['query'])
+
+        store.name = name
+        store.url_prefix = url_prefix
+        store.tag_name = tag_name
+        store.query = query
+        logger.debug(f"store: {store}")
+
+        store.save_to_mongo()
+
+        return redirect(url_for('.index'))
+
+    return render_template('stores/edit.html', store=store)
+
+
+@store_blueprint.route('/delete/<string:store_id>')
+def delete(store_id):
+    store = Store.get_by_id(store_id)
+    logger.debug(f"deleting store: {store}")
+    store.remove_from_mongo()
+    return redirect(url_for('.index'))
