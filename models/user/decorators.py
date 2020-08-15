899fecace73c717f1e4137868464c3713f851155
NikelausM <jnickm@gmail.com>
2020-08-15 15:35:43 -0600

    Add requires_login middleware decorator

---
+++ models/user/decorators.py
@@ -0,0 +1,12 @@
+import functools
+
+from typing import Callable
+from flask import session, flash, redirect, url_for, request
+
+
+def requires_login(f: Callable) -> Callable:
+    def decorated_function(*args, **kwargs):
+        if not session.get('email'):
+            flash("You need to be signed in for this page.", 'danger')
+            return redirect(url_for('users.login'))
+        return f(*args, **kwargs)
