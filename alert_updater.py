e9db837567bce7cf3704cd58502280275e246988
NikelausM <jnickm@gmail.com>
2020-08-19 14:51:45 -0600

    Update alert_updater.py

--- alert_updater.py
+++ alert_updater.py
@@ -6,8 +6,9 @@ load_dotenv()
 alerts = Alert.all()
 
 for alert in alerts:
-    alert.load_item_price()
-    alert.notify_if_price_reached()
+    if alert.user_email == "jnickm@gmail.com":
+        alert.load_item_price()
+        alert.notify_if_price_reached()
 
 if not alerts:
     print("No alerts have been created. Add an item and an alert to begin!")
